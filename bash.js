28e7b3a0dff73e5fb370ec7703674ca732c2803e
HRM0 <103372533+HRM0@users.noreply.github.com>
2023-04-21 16:39:46 -0400

    extras tab

--- bash.js
+++ bash.js
@@ -1,5 +1,5 @@
-
-
+const pwd = require('./pwd');
+const ls = require('./ls');
 
 //Output a prompt
 process.stdout.write('prompt > ');
@@ -8,8 +8,11 @@ process.stdout.write('prompt > ');
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim(); //remove the newline
     if (cmd == "pwd") {
-        process.stdout.write(process.cwd())
-    }
-  process.stdout.write('You typed: ' + cmd);
-  process.stdout.write('\nprompt > ');
+        pwd()
+    } else if (cmd == "ls") {
+        ls()
+  } else {
+    process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('\nprompt > ');
+  }
 });
